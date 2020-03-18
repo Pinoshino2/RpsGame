fb07b7e70d9a6427bd45602570469667c7f971a2
篠原裕矢 <pinoshino@shinoharayuuyanoMacBook-Pro.local>
2020-03-18 13:24:02 +0900

    MainActivityを軽くに記述

--- app/src/main/java/com/example/rpsgame/MainActivity.java
+++ app/src/main/java/com/example/rpsgame/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.rpsgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        この部分のコードの意味：
+//        IDによってビューオブジェクトを見つけて、ボタンオブジェクトに代入している。
+//        まだタップして値を取得する処理は書いていない。
+        Button gu = findViewById(R.id.gu);
+        Button choki = findViewById(R.id.choki);
+        Button pa = findViewById(R.id.pa);
     }
+
+    public void onButtonTap(View view) {
+        Intent intent = new Intent(this, ResultJudgeActivity.class);
+        intent.putExtra("MyChoice", view.getId());
+        startActivity(intent);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/rpsgame/ResultJudgeActivity.java
@@ -0,0 +1,5 @@
+package com.example.rpsgame;
+
+public class ResultJudgeActivity {
+//    ここに処理を書いていく
+}
