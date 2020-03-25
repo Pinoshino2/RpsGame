29683c67f1e7a5e60e7e98bc94f8e41dc8e593e5
篠原裕矢 <pinoshino@shinoharayuuyanoMacBook-Pro.local>
2020-03-25 11:33:34 +0900

    いったんセーブ

--- app/src/main/java/com/example/rpsgame/NameActivity.java
+++ app/src/main/java/com/example/rpsgame/NameActivity.java
@@ -2,7 +2,10 @@ package com.example.rpsgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class NameActivity extends AppCompatActivity {
 
@@ -11,4 +14,13 @@ public class NameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name);
     }
+
+//    public void sendMessage(View view) {
+//        Intent intent = new Intent(this, MainActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.edit_message);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+//
+//    }
 }
